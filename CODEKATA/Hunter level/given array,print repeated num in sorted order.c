b77077879bcbac7ebade5b998d3ef69fcb060cb2
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-03-10 11:57:42 +0530

    Create given array,print repeated num in sorted order.c

---
+++ CODEKATA/Hunter level/given array,print repeated num in sorted order.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    int n,a[n],b[n],k=0,i,j,temp=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d ",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{ 
+	    if(a[i]=='+')
+	    {
+	        continue;
+	    }
+	    for(j=i+1;j<n;j++)
+	    {
+	    if(a[i]==a[j])
+	    {
+	        b[k]=a[i];
+	        k++;
+	        a[j]='+';
+	    }
+	    }
+	}
+	for(i=0;i<k;i++)
+	{
+	    for(j=i+1;j<k;j++)
+	    {
+	        if(b[i]>b[j])
+	        {
+	            temp=b[i];
+	            b[i]=b[j];
+	            b[j]=temp;
+	        }
+	    }
+	    printf("%d ",b[i]);
+	}
+	return 0;
+}
