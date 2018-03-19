0521cddb2e4c985c29c249028e78821f2a5459f3
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-03-19 15:58:40 +0530

    Update given array,print repeated num in sorted order.c

--- CODEKATA/Hunter level/given array,print repeated num in sorted order.c
+++ CODEKATA/Hunter level/given array,print repeated num in sorted order.c
@@ -1,45 +1,54 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n,a[n],b[n],k=0,i,j,temp=0,count=0;
+    int n,a[10],b[10],k=0,i,j,temp=0,count=0;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
 	    scanf("%d ",&a[i]);
 	}
 	for(i=0;i<n;i++)
-	{ 
-	    if(a[i]=='+')
-	    {
-	        continue;
-	    }
+	{
 	    for(j=i+1;j<n;j++)
 	    {
-	    if(a[i]==a[j])
-	    {
-	        count=1;
-	        b[k]=a[i];
-	        k++;
-	        a[j]='+';
-	    }
-	    }
-	}
-	for(i=0;i<k;i++)
-	{
-	    for(j=i+1;j<k;j++)
-	    {
-	        if(b[i]>b[j])
+	        if(a[i]>a[j])
 	        {
-	            temp=b[i];
-	            b[i]=b[j];
-	            b[j]=temp;
+	            temp=a[i];
+	            a[i]=a[j];
+	            a[j]=temp;
 	        }
 	    }
-	    printf("%d ",b[i]);
+	    printf("%d ",a[i]);
 	}
-	if(count!=1)
+	printf("\n");
+	for(i=0;i<n-1;i++)
 	{
-	    printf("unique");
-	}
+	    count=0;
+	    for(j=i+1;j<n;j++)
+	    {
+	        if(a[i]==a[j])
+	        {
+	            a[j]='+';
+	            count=1;
+	        }
+	    }
+	    if(count==1)
+	    {
+	       b[k]=a[i];
+	       printf("%d ",b[k]);
+	       k++;
+	    }
+	}printf("\n");
+    if(k!=0)
+    {
+        for(i=0;i<k;i++)
+        {
+            printf("%d ",b[i]);
+        }
+    }
+    else
+    {
+        printf("unique");
+    }
 	return 0;
 }
