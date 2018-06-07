11b54a7d7b78d26310b973d296ce3aa75b154195
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-06-07 23:32:57 +0530

    Create a^b.java

---
+++ CODEKATA/Player level/a^b.java
@@ -0,0 +1,26 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Findpower {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int res=1;
+		for(int i=1;i<=b;i++)
+		{
+			res=res*a;
+		}
+		if(res==0)
+		{
+			System.out.print("no");
+		}
+		else
+		{
+			System.out.print(res);
+		}
+		sc.close();
+	}
+}
