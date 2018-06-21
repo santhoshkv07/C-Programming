d69c2975244625c9919f58d3db0006c4a56bebd9
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-06-21 21:49:37 +0530

    Update sort in descending order according to count of vowels in string array.java

--- CODEKATA/Player level/sort in descending order according to count of vowels in string array.java
+++ CODEKATA/Player level/sort in descending order according to count of vowels in string array.java
@@ -7,26 +7,27 @@ public class Descendingorderofvowelsinstrings
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		String[] s=new String[n+1];
+		String str=sc.nextLine();
+		int n=Integer.parseInt(str);
+		String[] s=new String[n];
 		String t="";
 		int count[]=new int[n];
 		int k=0,x=0;
-		for(int i=0;i<=n;i++)
-		{
-		    s[i]=sc.nextLine();
+		for(int i=0;i<n;i++)
+		{  
+			s[i]=sc.next();
 		}
-		for(int i=1;i<=n;i++)
+		for(int i=0;i<n;i++)
 		{
 		    for(int j=0;j<s[i].length();j++)
 		    {
-		        if(s[i].charAt(j)=='a'|s[i].charAt(j)=='e'|s[i].charAt(j)=='i'|s[i].charAt(j)=='o'|s[i].charAt(j)=='u'|s[i].charAt(j)=='A'|s[i].charAt(j)=='E'|s[i].charAt(j)=='I'|s[i].charAt(j)=='O'|s[i].charAt(j)=='U')
+		        if(s[i].charAt(j)=='a'||s[i].charAt(j)=='e'||s[i].charAt(j)=='i'||s[i].charAt(j)=='o'||s[i].charAt(j)=='u'||s[i].charAt(j)=='A'||s[i].charAt(j)=='E'||s[i].charAt(j)=='I'||s[i].charAt(j)=='O'||s[i].charAt(j)=='U')
 		        {
 		            x++;
 		        }
 		    }
 		    count[k++]=x;
-		    //System.out.print(count[k]+" "+x+"        ");
+		   // System.out.print(count[k]+" "+x+"        ");
 		    x=0;
 		}
 		for(int i=0;i<k;i++)
@@ -35,12 +36,12 @@ public class Descendingorderofvowelsinstrings
 		    {
 		        if(count[i]<count[j])
 		        {
-		            t=s[i+1];
-		            s[i+1]=s[j+1];
-		            s[j+1]=t;
+		            t=s[i];
+		            s[i]=s[j];
+		            s[j]=t;
 		        }
 		    }
-		    System.out.print(s[i+1]);
+		    System.out.print(s[i]);
 		    System.out.printf("\n");
 		}
 		sc.close();
